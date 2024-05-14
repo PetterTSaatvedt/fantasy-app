961ea0db755e1e1d3e81fca0e01aea1cf6202eff
PetterTSaatvedt <71778703+PetterTSaatvedt@users.noreply.github.com>
2024-05-14 22:02:30 +0200

    Component for Id input

---
+++ src/components/IdInput.js
@@ -0,0 +1,9 @@
+function IdInput() {
+    return (
+        <>
+            <input type="text" className="fpl-id" placeholder="Enter your FPL id"/>
+        </>
+    )
+}
+
+export default IdInput;
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,8 +1,10 @@
 import '../css/Main.css';
+import IdInput from './IdInput';
 
 function Main() {
     return (
         <div className="main-content">
+            <IdInput />
             <p>Nothing here yet.. Check back soon!</p>
         </div>
     )
