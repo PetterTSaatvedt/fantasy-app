7ebcd8337146d66fc26a9876a6f6d2108593c5f7
PetterTSaatvedt <71778703+PetterTSaatvedt@users.noreply.github.com>
2024-07-13 02:28:16 +0200

    Live Rank page layout

--- src/components/LiveRank.js
+++ src/components/LiveRank.js
@@ -1,9 +1,30 @@
+import '../css/LiveRank.css';
 
 function LiveRank(props) {
     return (
-        <>
-            <p>{props.fplId}</p>
-        </>
+        <main>
+            <div className="live-rank-team">
+                <div className="starting-xi">
+                    Starting XI
+                </div>
+                <div className="bench-players">
+                    Bench
+                </div>
+            </div>
+            
+            <div className="live-rank-stats">
+                <p>{props.fplId}</p>
+                <h2>Gameweek Statistics</h2>
+                <div className="live-rank-points">
+                    <p>Gameweek Points:</p>
+                </div>
+                <div className="live-rank-ranks">
+                    <p>Old Rank:</p>
+                    <p>Live Rank:</p>
+                    <p>Rank post-subs:</p>
+                </div>
+            </div>
+        </main>
     )
 }
 
