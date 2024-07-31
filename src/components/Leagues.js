e6c937e402dcff4a9bdf3dcc99ef51e1f8773605
PetterTSaatvedt <71778703+PetterTSaatvedt@users.noreply.github.com>
2024-07-31 20:48:03 +0200

    Leagues list

--- src/components/Leagues.js
+++ src/components/Leagues.js
@@ -1,17 +1,31 @@
+import '../css/Leagues.css';
+import { Link } from "react-router-dom";
 
 function Leagues(props) {
     const userData = props.userData;
     const userName = userData.player_first_name + ' ' + userData.player_last_name;
 
-    const leagueListItems = userData.leagues?.classic?.map(league => <li key={league.id}>{league.id}</li>)
+    const leagueListItems = userData.leagues?.classic?.map(league => 
+    <li key={league.id}>
+        <Link to={`/leagues/${league.id}`}>
+            <div className="leagues-list-item">
+                <h2>{league.name}</h2>
+                <p>1/15</p>
+            </div>
+        </Link>
+    </li>)
 
     return (
         <main>
-            <h1>{userName}'s Leagues</h1>
-            <div className="leagues-list">
-                <ul>
-                    {leagueListItems}
-                </ul>
+            <div className="leagues-wrapper">
+                <div className="leagues-header">
+                    <h1>{userName}'s Leagues</h1>
+                </div>
+                <div className="leagues-list">
+                    <ul>
+                        {leagueListItems}
+                    </ul>
+                </div>
             </div>
         </main>
     )
