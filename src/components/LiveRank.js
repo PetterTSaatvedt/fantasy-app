6c3221678694d39dffcf1d4ab9edc85aa705b738
PetterTSaatvedt <71778703+PetterTSaatvedt@users.noreply.github.com>
2024-11-29 19:33:24 +0100

    Added header for live rank page

--- src/components/LiveRank.js
+++ src/components/LiveRank.js
@@ -141,63 +141,71 @@ function LiveRank(props) {
 
     return forwards.length ? (
         <div className="live-rank-container">
-            <div className="live-rank-team">
-                <div className='live-rank-team-header'>
-                    <h1>{props.userData.name}</h1>
-                    <div className='gw-points'>
-                        <p className='gw-points-small'>Total Points:</p>
-                        <p className='gw-points-big'>{teamData.entry_history.points - teamData.entry_history.event_transfers_cost}</p>
-                        <p className='gw-points-small'>{teamData.entry_history.event_transfers_cost > 0 && `(${teamData.entry_history.points} - ${teamData.entry_history.event_transfers_cost}) `}</p>
-                    </div>
-                </div>
-                <div className="starting-xi">
-                    <div className="starting-keeper">
-                        <PlayerCard 
-                            name={goalkeeper.name} 
-                            points={goalkeeper.total_points} 
-                            team={goalkeeper.team} 
-                            points_explained={goalkeeper.points_explained}
-                            is_captain={goalkeeper.is_captain}
-                            is_vice_captain={goalkeeper.is_vice_captain}
-                            multiplier={goalkeeper.multiplier}
-                        />
-                    </div>
-                    <div className="starting-defenders">
-                       {renderPlayers(defenders)}
-                    </div>
-                    <div className="starting-midfielders">
-                        {renderPlayers(midfielders)}
-                    </div>
-                    <div className="starting-forwards">
-                        {renderPlayers(forwards)}
-                    </div>
-                </div>
-                <div className="bench-players">
-                    {renderPlayers(bench)}
-                </div>
-            </div>
-            
-            <div className="live-rank-stats">
+            <div className='live-rank-header'>
+                <h1>Live Rank - Gameweek {props.userData.current_event}</h1>
                 <div className="personal-info">
-                    <h2>{userName}</h2>
-                    <p>Years active: {props.userData.years_active}</p>
-                    <p>Country: {props.userData.player_region_name}</p>
-                    <p>Overall points: {props.userData.summary_overall_points}</p>
+                    <img src={`https://flagcdn.com/${props.userData.player_region_iso_code_short.toLowerCase()}.svg`} width={30} />
+                    <p>{userName}</p>
                 </div>
-                <div className="stats">
-                    <h2>Gameweek {props.userData.current_event} Statistics</h2>
-                    <div className="live-rank-points">
-                        <p>Gameweek Points: {teamData.entry_history.points}</p>
+                <button>Change ID</button>
+            </div>
+
+            <div className='team-and-stats'>
+                <div className="live-rank-team">
+                    <div className='live-rank-team-header'>
+                        <h2>{props.userData.name}</h2>
+                        <div className='gw-points'>
+                            <p className='gw-points-small'>Total Points:</p>
+                            <p className='gw-points-big'>{teamData.entry_history.points - teamData.entry_history.event_transfers_cost}</p>
+                            <p className='gw-points-small'>{teamData.entry_history.event_transfers_cost > 0 && `(${teamData.entry_history.points} - ${teamData.entry_history.event_transfers_cost}) `}</p>
+                        </div>
                     </div>
-                    <div className="live-rank-ranks">
-                        <p>Gameweek Rank: {props.userData.summary_event_rank.toLocaleString()}</p>
-                        <p>Previous Rank: {props.userData.leagues.classic[3].entry_last_rank.toLocaleString()}</p>
-                        <p>New Rank: {''}
-                            {props.userData.summary_overall_rank.toLocaleString()}
-                            {props.userData.summary_overall_rank < props.userData.leagues.classic[3].entry_last_rank ? ' ⬆️'
-                            : props.userData.summary_overall_rank > props.userData.leagues.classic[3].entry_last_rank ? ' 🔻'
-                            : ' 🔁'}
-                        </p>
+                    <div className="starting-xi">
+                        <div className="starting-keeper">
+                            <PlayerCard 
+                                name={goalkeeper.name} 
+                                points={goalkeeper.total_points} 
+                                team={goalkeeper.team} 
+                                points_explained={goalkeeper.points_explained}
+                                is_captain={goalkeeper.is_captain}
+                                is_vice_captain={goalkeeper.is_vice_captain}
+                                multiplier={goalkeeper.multiplier}
+                            />
+                        </div>
+                        <div className="starting-defenders">
+                        {renderPlayers(defenders)}
+                        </div>
+                        <div className="starting-midfielders">
+                            {renderPlayers(midfielders)}
+                        </div>
+                        <div className="starting-forwards">
+                            {renderPlayers(forwards)}
+                        </div>
+                    </div>
+                    <div className="bench-players">
+                        {renderPlayers(bench)}
+                    </div>
+                </div>
+                
+                <div className="live-rank-stats">
+                    
+                    <div className="stats">
+                        <div className="live-rank-points">
+                            <h2>Points</h2>
+                            <p>Gameweek Points: {teamData.entry_history.points}</p>
+                            <p>Overall points: {props.userData.summary_overall_points}</p>
+                        </div>
+                        <div className="live-rank-ranks">
+                            <h2>Ranks</h2>
+                            <p>Live Rank: {''}
+                                {props.userData.summary_overall_rank.toLocaleString()}
+                                {props.userData.summary_overall_rank < props.userData.leagues.classic[3].entry_last_rank ? ' ⬆️'
+                                : props.userData.summary_overall_rank > props.userData.leagues.classic[3].entry_last_rank ? ' 🔻'
+                                : ' 🔁'}
+                            </p>
+                            <p>Previous Rank: {props.userData.leagues.classic[3].entry_last_rank.toLocaleString()}</p>
+                            <p>Gameweek Rank: {props.userData.summary_event_rank.toLocaleString()}</p>
+                        </div>
                     </div>
                 </div>
             </div>
